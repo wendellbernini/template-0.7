6405333fe80db2145fc4252f33e0915bd6a51ed3
vboxuser <vboxuser@Xubuntu.myguest.virtualbox.org>
2024-12-01 15:23:08 -0300

    Build and deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,25 @@ module.exports = {
   plugins: ['prettier'], // Adiciona Prettier como plugin
   rules: {
     'prettier/prettier': 'error', // Marca como erro qualquer desvio das regras do Prettier
+    // Limita a complexidade de funções/métodos
+    complexity: ['error', 5],
+
+    // Limita o número de linhas em uma função/arquivo
+    'max-lines': [
+      'error',
+      { max: 50, skipBlankLines: true, skipComments: true },
+    ],
+
+    // Limita o número de declarações em uma função
+    'max-statements': ['error', 10],
+
+    // Previne o uso de números "mágicos"
+    'no-magic-numbers': ['error', { ignore: [0, 1] }],
+
+    // Evita ternários aninhados
+    'no-nested-ternary': 'error',
+
+    // Limita o número de linhas em branco consecutivas
+    'no-multiple-empty-lines': ['error', { max: 1 }],
   },
 };
